4c79b6e459c43f2139b011f1baae3e67089f8490
Niramay Thaker <thakerniramay@gmail.com>
2023-02-02 18:14:34 +0530

    User getting added to default group Automatically

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,8 +3,6 @@
 	manage.py,3\1\3156ad13e4d695cd526bbb7b031016ecba842270
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-<
-main/apps.py,1\0\10550cb9be3f72f7048fb0806ee3ed9ce8ef5af5
 T
 $access_management_system/settings.py,a\0\a0554c11a80de718cef0f100e5dcb3ada3a7f8e2
 P
--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class MainConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "main"
+
+    def ready(self):
+        # Has to import inside so it gets load every time when function is triggered
+        from django.contrib.auth.models import Group
+        from django.db.models.signals import post_save
+
+        # Will take to arguments **kwargs has user info
+        def add_to_default_group(sender, **kwargs):
+
+            user = kwargs['instance']
+            if kwargs['created']:
+                group, ok = Group.objects.get_or_create(name="default")
+                group.user_set.add(user)
+
+        post_save(add_to_default_group, sender=settings.AUTH_USER_MODEL)
